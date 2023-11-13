e14f80a92bb126766c11f1af2de2ef4bd34eaceb
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-11-13 21:24:11 +0600

    chartbox added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,6 @@
-import TopDeals from "../../components/topDeals/topDeals";
+import ChartBox from "../../components/chartBox.tsx/ChartBox";
+import TopDeals from "../../components/topDeals/TopDeals";
+import { chartBoxConversion, chartBoxProduct, chartBoxRevenue } from "../../utils/data/Data";
 import "./home.scss";
 
 const Home = () => {
@@ -7,11 +9,19 @@ const Home = () => {
       <div className="box box1">
         <TopDeals></TopDeals>
       </div>
-      <div className="box box2">Box2</div>
-      <div className="box box3">Box3</div>
+      <div className="box box2">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct}/>
+      </div>
       <div className="box box4">Box4</div>
-      <div className="box box5">Box5</div>
-      <div className="box box6">Box6</div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion}/>
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue}/>
+      </div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
       <div className="box box9">Box9</div>
